bb045d9a59a50dc34b2230d72d25ab1f76eaa4a4
AlexSandr90 <gayrsuk@gmail.com>
2018-07-25 15:25:11 +0300

    my first commit in this rep

---
+++ js/main.js
@@ -0,0 +1,30 @@
+let years           = document.getElementById('years');
+let months          = document.getElementById('months');
+let days            = document.getElementById('days');
+let plans           = document.getElementById('plans');
+let write           = document.getElementById('write');
+let print           = document.getElementById('print');
+let out             = document.getElementById('output');
+
+
+
+let base = {};
+
+write.addEventListener('click', function () {
+    base.first = plans.value;
+    plans.value = ' ';
+});
+
+print.addEventListener('click', function () {
+    out.innerHTML = base.first;
+    plans.value = ' ';
+
+    out.classList.toggle('output');
+    // if (plans.value == '') {
+    //     out.classList.remove('output');
+    // }
+    //     out.classList.add('output');
+});
+
+
+
